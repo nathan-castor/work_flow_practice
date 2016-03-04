85a47c7f2ba7c2732580a7be6ab9aa695fe7b1fd
Nate Castor <nathancastor@gmail.com>
2016-03-04 12:29:29 -0800

    added blank root route

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
 
+app.get('/',function(req,res) {
+  //nothing yet
+})
+
 app.listen(3000, function(){
   console.log("listening on port 3000")
 })
