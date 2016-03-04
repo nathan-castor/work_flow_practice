8aaa794b19372fd40e232574bb526d4795b6d379
Joe Villarba <socagambit@aol.com>
2016-03-04 12:25:59 -0800

    fixed typo

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
 
-app.listener(3000, function(){
+app.listen(3000, function(){
   console.log("listening on port 3000")
 })
